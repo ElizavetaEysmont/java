8159e5800507fffbc49be6aa9444069d47f52be5
ElizavetaEysmont <sirotens@gmail.com>
2023-12-27 22:39:55 +0300

    lab5

--- src/src/ru/itmo/java/basics/practic5/method2.java
+++ src/src/ru/itmo/java/basics/practic5/method2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class method2 {
     public static boolean isPalindrome (String word){
+        word = word.toLowerCase();
         int i = 0;
         int j = word.length()-1;
         while (i<j){
--- src/src/ru/itmo/java/basics/practic5/method3.java
+++ src/src/ru/itmo/java/basics/practic5/method3.java
@@ -3,13 +3,18 @@ package src.ru.itmo.java.basics.practic5;
 import java.util.Scanner;
 
 public class method3 {
-    public static String censorWord(String text){
-        return text.replace("бяка","вырезанно цензурой");
+    public static String censorWord(String text, String searchWord, String replaceWord){
+        return text.replaceAll("(?i)" + searchWord, replaceWord);
     }
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
         String text = scanner.nextLine();
-        String censorText = censorWord(text);
+        System.out.println("Введите слово для замены: ");
+        String searchWord = scanner.nextLine();
+        System.out.println("Введите слово, на которое надо заменить: ");
+        String replaceWord = scanner.nextLine();
+        String censorText = censorWord(text, searchWord, replaceWord);
         System.out.println(censorText);
     }
 }
